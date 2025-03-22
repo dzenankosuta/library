e714cc7ad25281c83ff393295eca7092e41748fd
dzenankosuta <dzenan_kosuta@hotmail.com>
2025-03-22 10:30:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main style={{minHeight: '65vh'}}> 
+
+      </main>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+function Footer() {
+  return (
+    <div className='footer'></div>
+  )
+}
+
+export default Footer
