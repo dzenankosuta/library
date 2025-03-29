f5d22fc01effa5f88dfa0d0379afee341be8de2e
dzenankosuta <dzenan_kosuta@hotmail.com>
2025-03-29 10:38:15 +0100

    add react router dom implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { Routes, Route } from "react-router"
+import Home from './pages/Home'
+import Books from './pages/Books'
 
 function App() {
   return (
     <div className="app-container">
       <Header/>
       <main>
-        
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/books' element={<Books/>} />
+        </Routes>
       </main>
       <Footer/>
     </div>
