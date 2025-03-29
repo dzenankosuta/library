8ffd956b8b4b60ceb840989b5ebb9be7f69e24ca
dzenankosuta <dzenan_kosuta@hotmail.com>
2025-03-29 10:39:12 +0100

    add new logo and navigation in header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import "./index.css"
+import { NavLink } from 'react-router-dom'
+import logo from "../../assets/logo.png"
 
 function Header() {
   return (
     <header className='header'>
       <div className="logo">
-        <span className="logo-text">📚 Biblioteka</span>
+      <NavLink to={'/'}>
+        <img src={logo} height="80" />
+        </NavLink>
       </div>
       <nav className="nav-links">
-        <a href="#" className="nav-link">Početna</a>
-        <a href="#" className="nav-link">Knjige</a>
+        <NavLink to={'/'} className="nav-link">Početna</NavLink>
+        <NavLink to={'/books'} className="nav-link">Knjige</NavLink>
       </nav>
     </header>
   )
