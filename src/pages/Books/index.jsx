1f334f4fb234adccd3f6c06c93ae86a47c8681e5
dzenankosuta <dzenan_kosuta@hotmail.com>
2025-04-05 14:58:21 +0200

    update Books page

--- src/pages/Books/index.jsx
+++ src/pages/Books/index.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BookCard from '../../components/cards/BookCard'
+import "./index.css"
+import booksJSON from "../../common/books.json"
 
 function Books() {
+  const [books, setBooks] = useState(booksJSON)
+  const handleEditBook = () => {
+    console.log("Editovanje knjige")
+  }
+  const handleDeleteBook = () => {
+    console.log("Brisanje knjige")
+  }
   return (
-    <div>Books</div>
+    <div>
+      <div className='grid-container'>
+        {
+          books.map((book) => {
+            return (
+            <BookCard 
+              key={book.id} 
+              bookName={book.bookName} 
+              author={book.author} 
+              year={book.year}
+              genre={book.genre}
+              onEdit={() => handleEditBook()}
+              onDelete={() => handleDeleteBook()}
+              />)
+          })
+        }
+      </div>
+    </div>
   )
 }
 
