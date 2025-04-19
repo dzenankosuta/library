9828a6e87ff1f7df1029b99db3596caa904cf484
dzenankosuta <dzenan_kosuta@hotmail.com>
2025-04-19 10:33:24 +0200

    show confirm delete modal

--- src/pages/Books/index.jsx
+++ src/pages/Books/index.jsx
@@ -2,21 +2,42 @@ import React, { useState } from 'react'
 import BookCard from '../../components/cards/BookCard'
 import "./index.css"
 import booksJSON from "../../common/books.json"
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 function Books() {
   const [books, setBooks] = useState(booksJSON)
   const handleEditBook = () => {
     console.log("Editovanje knjige")
   }
-  const handleDeleteBook = () => {
-    console.log("Brisanje knjige")
+  const handleDeleteBook = (id) => {
+    const filteredBooks = books.filter((book) => book.id !== id)
+    setBooks(filteredBooks)
   }
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div>
-      <div className='grid-container'>
-        {
-          books.map((book) => {
+      <div>
+{
+  books.length === 0 ? (
+    <div><p>Trenutno nema knjiga.</p></div>
+  ) : (
+    <div className='grid-container'>
+      {books.map((book) => {
             return (
             <BookCard 
               key={book.id} 
@@ -24,13 +45,37 @@ function Books() {
               author={book.author} 
               year={book.year}
               genre={book.genre}
-              onEdit={() => handleEditBook()}
-              onDelete={() => handleDeleteBook()}
+              onEdit={() => handleClickOpen()}
+              onDelete={() => handleDeleteBook(book.id)}
               imageClassName="book-image"
               />)
-          })
-        }
+            })}
+    </div>
+  )
+}
       </div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous
+            location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={handleClose} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
